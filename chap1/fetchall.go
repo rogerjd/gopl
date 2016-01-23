5dfb181a03b25b5bc5aba8f77324092aae3a7f5a
r d <rjpd@netzero.com>
2016-01-22 20:48:01 -0500

    fetchall

--- chap1/fetchall.go
+++ chap1/fetchall.go
@@ -2,24 +2,41 @@ package chap1
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
+	"time"
 )
 
 func fetchall(urls []string) {
+	start := time.Now()
+	ch := make(chan string)
+
 	for _, url := range urls {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "fetch: %v\n", err)
-			return
-		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			return
-		}
-		fmt.Printf("%s", b)
+	    println(url)
+		go fetchOne(url, ch) // start a goroutine
 	}
+
+	for range urls {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetchOne(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body) // dev/null
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
 }
